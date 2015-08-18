75f452b981e13fc6fa5a452201c5c3c374709c49
SILL-BILL <7th5jack@gmail.com>
2015-08-18 20:38:47 +0900

    none

--- threejs/threejs_junkyard/sample-002/main.js
+++ threejs/threejs_junkyard/sample-002/main.js
@@ -24,7 +24,7 @@ function init() {
 	// -------------
 
 	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-	camera.position.z = 400;
+	camera.position.z = 900;
 
 	scene = new THREE.Scene();
 
