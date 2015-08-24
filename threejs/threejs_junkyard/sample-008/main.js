6d3826f3647685a31a01af0ad8ee5143cb5c9762
SILL-BILL <7th5jack@gmail.com>
2015-08-24 13:37:13 +0900

    none

--- threejs/threejs_junkyard/sample-008/main.js
+++ threejs/threejs_junkyard/sample-008/main.js
@@ -28,8 +28,8 @@ function init() {
 	// camera
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 
-	camera.position.set( 0, 0, 500 );
-	camera.up.set(0, 1, 0);
+	camera.position.set(0, 0, 500);
+	camera.up.set(0, 0, 1);
 //	controls = new THREE.DeviceOrientationControls(camera);
 
 	scene = new THREE.Scene();
