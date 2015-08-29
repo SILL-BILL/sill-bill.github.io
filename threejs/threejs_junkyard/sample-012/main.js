3a0fe56660d936693086673c9e3fecb4999f2a0d
SILL-BILL <7th5jack@gmail.com>
2015-08-29 16:10:10 +0900

    none

--- threejs/threejs_junkyard/sample-012/main.js
+++ threejs/threejs_junkyard/sample-012/main.js
@@ -36,12 +36,12 @@ function init() {
 	// camera
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 
-//	camera.position.set(300, 300, 300);
-//	camera.lookAt({x:0,y:0,z:0});
+	camera.position.set(300, 300, 300);
+	camera.lookAt({x:0,y:0,z:0});
 
 	//オービット
 	controls = new THREE.OrbitControls(camera);
-	controls.autoRotate();
+	controls.autoRotate = true;
 
 	//デバイス
 //	controls = new THREE.DeviceOrientationControls(camera);
