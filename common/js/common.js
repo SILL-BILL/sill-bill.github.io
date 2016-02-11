9c1d98ac076e21d73ea957dccd62abc445cf56b9
SILL-BILL <7th5jack@gmail.com>
2016-02-11 16:38:14 +0900

    none

--- common/js/common.js
+++ common/js/common.js
@@ -20,8 +20,11 @@ $(function(){
 // window onload
 $(window).on('load', function(){
 
+	//ローディングカバー
+	$('.loading-cover').velocity({opacity:0},{delay:500*1,duration:500,easing:'ease-out',display:'none'}).addClass('off');
+
 	//ヘッダーロゴアニム
-	$('.header .navbar .logo').velocity({opacity:0, rotateX:'-90deg'},{duration:1}).velocity({opacity:1,rotateX:'0'},{delay:500*3, duration:500*2,easing:'ease-out'}).attr('style','');
+	$('.header .navbar .logo').velocity({opacity:0, rotateX:'-90deg'},{duration:1}).velocity({opacity:1,rotateX:'0'},{delay:500*2, duration:500*2,easing:'ease-out'}).attr('style','');
 
 	/*------------------
 	 heightline item-list
